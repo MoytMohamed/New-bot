eba0831becdd263afefe694c23cb5c908a5a2151
MoytMohamed <123094825+MoytMohamed@users.noreply.github.com>
2024-07-09 10:57:29 +0300

    Update gp-warning.js

--- plugins/gp-warning.js
+++ plugins/gp-warning.js
@@ -4,45 +4,44 @@ let handler = async (m, { conn, text, args, groupMetadata, usedPrefix, command }
   if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false
   else who = m.chat
   if (!who) throw `✳️ Tag or mention someone\n\n📌 Example : ${usedPrefix + command} @user`
-  if (!(who in global.db.data.users)) throw `✳️ The user is not found in my database`
+  if (!(who in global.db.data.users)) throw `*❞لــم أجــد اي بــيـانـات لــهذا الــمـسـتـخدم !❝*`
   let name = conn.getName(m.sender)
   let warn = global.db.data.users[who].warn
   if (warn < war) {
     global.db.data.users[who].warn += 1
     m.reply(
       `
-⚠️ *Warned User* ⚠️
+ *──── ╯ إنــذار ╭ ────*
 
-▢ *Admin:* ${name}
-▢ *User:* @${who.split`@`[0]}
-▢ *Warns:* ${warn + 1}/${war}
-▢ *Reason:* ${text}`,
+*--⧽ الــمـشـرف:* ${name}
+*--⧽ الــشـخص:* @${who.split`@`[0]}
+*--⧽ الإنــذارات:* ${warn + 1}/${war}
+*--⧽ الــسبب:* ${text}`,
       null,
       { mentions: [who] }
     )
     m.reply(
       `
-⚠️ *caution* ⚠️
-You received a warning from an admin
+*──── ╯ إنــذار ╭ ────*
+*لــقد تــلـقيـت إنذاراً مــن احـد الــمـشرفــين !*
 
-▢ *Warns:* ${warn + 1}/${war} 
-if you receive *${war}* warnings you will be automatically removed from the group`,
+*--⧽ عــدد الإنــذارات :* ${warn + 1}/${war}`,
       who
     )
   } else if (warn == war) {
     global.db.data.users[who].warn = 0
-    m.reply(`⛔ The user exceeded the *${war}* warnings will therefore be removed`)
+    m.reply(`*ســيـتم طــرد الــعضـو لأنــه قـد تــخطـى ${war} مــن الإنــذارات !*`)
     await time(3000)
     await conn.groupParticipantsUpdate(m.chat, [who], 'remove')
     m.reply(
-      `♻️ You were removed from the group *${groupMetadata.subject}* because you have been warned *${war}* times`,
+      `لــقد تــم ازالــتك مــن هــذا الــجروب *${groupMetadata.subject}* لأنــك قــد تــخـطـيت هذا الــعدد مــن الإنــذارات *${war}*`,
       who
     )
   }
 }
 handler.help = ['warn @user']
 handler.tags = ['group']
-handler.command = ['warn']
+handler.command = ['انذار', 'إنذار']
 handler.group = true
 handler.admin = true
 handler.botAdmin = true
